0cf3c4426eb95f1c5cb4aea77875fc0e8e1c8c23
ahming <toahming@126.com>
2020-09-07 13:27:03 +0800

    移除base-common子项目，因为它已被改为独立部署依赖

--- base-common/src/main/java/com/websocket/service/CommandHistoryService.java
+++
@@ -1,8 +0,0 @@
-
-package com.websocket.service;
-
-/**
- * WebSocket指令历史服务
- */
-public interface CommandHistoryService {
-}
--- build-all-projects.sh
+++ build-all-projects.sh
@@ -2,5 +2,7 @@
 
 # build_type 有 release 或 test 等
 
-cd base-common; ./gradlew clean build -P build_type=release; cd ..
+# 改为独立发布依赖
+# cd base-common; ./gradlew clean build -P build_type=release; cd ..
+
 cd websocket-teaching; ./gradlew clean build -P build_type=release; cd ..
